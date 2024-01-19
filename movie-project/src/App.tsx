b88b549552ce886f7942a9b410a44fab3465d26b
matia-lee <matialee9@gmail.com>
2024-01-19 10:58:00 -0500

    frontend

--- movie-project/src/App.tsx
+++ movie-project/src/App.tsx
@@ -1,26 +1,119 @@
 import React from 'react';
-import logo from './logo.svg';
+import MovieCard from './MovieCard';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const api_key = process.env.REACT_APP_TMDB_API;
+
+// const options = {
+  // method: 'GET',
+  // headers: {
+  // accept: 'application/json',
+  // Authorization: 'Bearer ' + api_key
+  // }
+// };
+
+  
+// fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US', options)
+// .then(response => response.json())
+// .then(response => console.log(response))
+// .catch(err => console.error(err));
+
+  
+// const App = () => {
+// const [movies, setMovies] = useState([]);
+// const [searchTerm, setSearchTerm] = useState('');
+// const searchMovies = async (title:string) => {
+// const response = await fetch(`${API_URL}&s=${title}`);
+// const data = await response.json();
+// setMovies(data.Search);
+// }
+
+
+// useEffect(() => {
+// searchMovies('Everything Everywhere All At Once');
+// },[])
+
+
+// return (
+// <div className = 'app'>
+// <h1>MovieLand</h1>
+// <div className = "search">
+// <input
+// placeholder="Search for movies"
+// value = {searchTerm}
+// onChange = {(e) => setSearchTerm(e.target.value)}
+// />
+
+// <img
+// src={SearchIcon}
+// alt="search"
+// onClick = {() => searchMovies(searchTerm)}
+// />
+
+// </div>
+
+// {movies?.length > 0 ? (
+// <div className = "container">
+// {movies.map((movie) => (
+// <MovieCard key = {movie.id} movie={movie} />
+// ))}
+// </div>
+// ) :(
+// <div className = "empty">
+// <h2></h2>
+// </div>
+// )
+// }
+// </div>
+// );
+// }
+
+// export default App;
+
+  
+
+interface Movie {
+id: number;
+}
+
+const App = () => {
+
+// const [movies, setMovies] = useState([]);
+const [movies, setMovies] = useState<Movie[]>([]);
+
+
+useEffect(() => {
+  const api_key = process.env.REACT_APP_TMDB_KEY;
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer ' + api_key
+  }};
+
+  fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US', options)
+    .then(response => response.json())
+    .then(data => {
+      setMovies(data.results);
+    })
+    .catch(err => console.error(err));
+}, []);
+
+
+
+return(
+  <div className = "app">
+    <h1 className = "main-text">Cineberg</h1>
+      <div className = "container">
+
+        {movies.map((movie) => (
+        <MovieCard key = {movie.id} movie={movie} />
+        ))}
+
+      </div>
+  </div>
+);
 }
 
 export default App;
---
+++ movie-project/src/MovieCard.jsx
@@ -0,0 +1,26 @@
+
+
+const MovieCard = ({ movie }) => {
+    return (
+    <div className = "movie">
+        <div>
+            <p>{movie.release_date}</p>
+        </div>
+
+        <div>
+            <img
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt={movie.title}
+            style={{ width: '100%', height: '100%' }}
+            />
+        </div>
+
+        <div>
+            <span>{movie.media_type}</span>
+            <h3>{movie.title}</h3>
+        </div>
+    </div>
+);
+}
+
+export default MovieCard;
