f45ba918f9fdc9810084efac424f26871cb5722d
matia-lee <matialee9@gmail.com>
2024-02-14 12:19:30 -0500

    grabbing article titles and urls

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
 
@@ -10,23 +12,38 @@ driver = webdriver.Chrome(service=service)
 
 load_dotenv(".env")
 
-# news_lists = driver.find_elements(By.CSS_SELECTOR, ".o-tease-list__item")
-
-# for list in news_lists:
-#     free_content = list.find_elements(By.CSS_SELECTOR, "a.c-span__link.u-color-pale-sky-2")
-    
-#     if free_content:
-#         title = list.find_element(By.CSS_SELECTOR, "h3")
-#         print(title.text)
-
 driver.get(os.environ.get("website_url"))
+
+# WebDriverWait(driver, 5).until(
+#     EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".cards_under-hero__V5y6G, .cards_cards-container__HiYvz"))
+# )
 
 news_lists_1 = driver.find_elements(By.CSS_SELECTOR, ".cards_under-hero__V5y6G")
 news_lists_2 = driver.find_elements(By.CSS_SELECTOR, ".cards_cards-container__HiYvz")
 
+article_titles = []
+article_urls = []
+
 for item in news_lists_1 + news_lists_2:
     titles = item.find_elements(By.CSS_SELECTOR, ".card_title__I1a3A")
+
     for title in titles:
-        print (title.text)
+        article_titles.append(title.text)
+
+    links = item.find_elements(By.CSS_SELECTOR, "a")
+    
+    for link in links:
+        url = link.get_attribute("href")
+        article_urls.append(url)
+
+# for url in article_urls:
+#     driver.get(url)
+#     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='article-content']")))
+
+
+print(article_titles, article_urls)
+    
+
+
 
 driver.quit()
