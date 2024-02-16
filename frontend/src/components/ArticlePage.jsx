68a59dae33adc5d85933a28561c556316bd043a9
matia-lee <matialee9@gmail.com>
2024-02-16 00:04:44 -0500

    added back button to news article content

--- frontend/src/components/ArticlePage.jsx
+++ frontend/src/components/ArticlePage.jsx
@@ -1,12 +1,21 @@
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ArticlePage = () => {
 
     const location = useLocation();
+    const navigate = useNavigate();
     const { content } = location.state;
+
+    const handleBackClick = () => {
+        navigate("/news");
+    };
 
     return (
         <div>
+            <div className="back-button" onClick={handleBackClick}>
+                <span className="back-arrow">&lt;</span>
+            </div>
            <p>{content}</p>
         </div>
     );
--- frontend/src/components/MovieNews.jsx
+++ frontend/src/components/MovieNews.jsx
@@ -37,7 +37,6 @@ const MovieNews = () => {
       <div className="news-container">
         {news.map((item, index) => (
           <div key={index} className="news-articles" onClick={() => handleArticleClick(item.title, item.content)}>
-            {/* <h3>{item.content}</h3> */}
             <img src={item.image} alt={item.title} />
             <div className="text-container">
               <h4>-Movie News-</h4>
